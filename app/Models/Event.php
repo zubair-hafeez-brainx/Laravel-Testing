321a38046b608263f3523330b584ee274c209f2d
Zubair Hafeez <zubair.hafeez@brainxtech.com>
2022-10-26 09:33:56 +0500

    Added comment

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -7,6 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Event extends Model
 {
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     * For getting the nested childrens
+     */
     public function children()
     {
         return $this->hasMany('MenuItem')->with('children');
